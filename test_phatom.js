095003b5f2b726243ce545957090cee8c40d5abf
duykhanh <duykhanh1983@gmail.com>
2017-09-13 14:17:04 +0700

    Update test_phatom.js

--- test_phatom.js
+++ test_phatom.js
@@ -14,6 +14,25 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname));
 
+
+var Crawler = require("crawler");
+
+var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        //done();
+    }
+});
+c.queue('https://eth.remitano.com/vn');
 app.get('/', function (req, res, next) {
     
     res.sendFile(__dirname + '/index.html');
