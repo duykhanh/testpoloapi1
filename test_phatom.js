310ff170b0e5f1d4e68e00b33ba3643cf1f5b46f
duykhanh <duykhanh1983@gmail.com>
2017-09-13 13:48:56 +0700

    Update test_phatom.js

--- test_phatom.js
+++ test_phatom.js
@@ -1,3 +1,10 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+var express = require('express');
+var server = require('http').createServer(app);
+var app = express();
+var port = 3000;
+
+server.listen(process.env.PORT || port);
 var phantom = require('phantom');
 
     phantom.create().then(function(ph) {
