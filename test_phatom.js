7105b73940d810c69a6caf757122fa3f256f434d
duykhanh <duykhanh1983@gmail.com>
2017-09-15 13:10:56 +0700

    Update test_phatom.js

--- test_phatom.js
+++ test_phatom.js
@@ -63,6 +63,41 @@ app.get('/', function (req, res, next) {
 var port = 3000;
 
 server.listen(process.env.PORT || port);
+var Horseman = require('node-horseman');
+const config = {
+  browser: {
+    clientScripts: [],
+    timeout: 5000,
+    interval: 50,
+    loadImages: true,
+    switchToNewTab: false,
+    // no support from SlimerJS cookiesFile: null,
+    // no support from SlimerJS ignoreSSLErrors: false,
+    sslProtocol: 'any',
+    // no support from SlimerJS webSecurity: true,
+    injectJquery: true,
+    injectBluebird: false,
+    bluebirdDebug: false,
+    // no support from SlimerJS proxy: null,
+    // no support from SlimerJS proxyType: null,
+    // no support from SlimerJS proxyAuth: null,
+    phantomPath: '/usr/local/Cellar/slimerjs/0.10.0/bin/slimerjs',
+    debugPort: null,
+    debugAutorun: true,
+  },
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.4.10 (KHTML, like Gecko) Version/8.0.4 Safari/600.4.10',
+}
+const horseman = new Horseman(config.browser);
 
+horseman
+  .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+  .open('http://www.google.com')
+  .type('input[name="q"]', 'github')
+  .click('[name="btnK"]')
+  .keyboardEvent('keypress', 16777221)
+  .waitForSelector('div.g')
+  .count('div.g')
+  .log() // prints out the number of results
+  .close();
 
 
